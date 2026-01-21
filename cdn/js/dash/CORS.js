947bfac538fe875b6c74c9cfbc2e8bf1dbc3da9d
JensTech <jensonguffick@yahoo.com>
2026-01-21 15:53:30 +0000

    Removed an api that is disallowed now

--- cdn/js/dash/CORS.js
+++ cdn/js/dash/CORS.js
@@ -13,7 +13,6 @@
 const DEBUG = false; // MAKE ABSOLUTELY SURE THIS IS FALSE BEFORE PUSHING. It just shows debug logs
 
 const WORKERS = [
-    'https://api.classchartspro.qzz.io',
     'https://api.classchartspro2.workers.dev/'
 ];
 
